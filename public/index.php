6f540f532186c73317be976574b5cb43f0e0f37c
angel coyasos <angelcotr@gmail.com>
2025-07-27 12:41:30 -0500

    fix

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php 
 
+// Configuración de errores para producción
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
-error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/../logs/php_error.log'); // Ruta al archivo de log
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_WARNING); // Puedes ajustar según lo que quieras registrar
+
 
 require_once __DIR__ . '/../includes/app.php';
 
